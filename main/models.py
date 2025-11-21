bef09f1038f92a10768ba2915166addf337f77e8
Muhammad-amin7 <abduhalilovshohruh681@gmail.com>
2025-11-21 09:39:29 +0500

    image inspection

--- main/models.py
+++ main/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from .validators import validate_image
 
 
 # Create your models here.
@@ -12,14 +13,14 @@ class Course(models.Model):
     lesson_time = models.DecimalField(max_digits=3, decimal_places=1, default=2)
     lesson_duration = models.PositiveIntegerField(default=10)
     number_of_students = models.PositiveIntegerField(default=12)
-    type_of_education = models.CharField(max_length=36 , default="Offline")
+    type_of_education = models.CharField(max_length=36, default="Offline")
     level = models.CharField(max_length=48)
     language = models.CharField(
         max_length=36, choices=language_choices, default="Uzbek"
     )
-    banner = models.ImageField(upload_to="course_banners/")
+    banner = models.ImageField(upload_to="course_banners/", validators=[validate_image])
     price = models.PositiveIntegerField(default=0)
-    logo = models.ImageField(upload_to="course_logos/")
+    logo = models.ImageField(upload_to="course_logos/", validators=[validate_image])
 
     def __str__(self):
         return self.title
@@ -41,22 +42,22 @@ class CourseDescription(models.Model):
 
 
 class CourseIcon(models.Model):
-    icon = models.ImageField(upload_to="course_icons/")
+    icon = models.ImageField(upload_to="course_icons/", validators=[validate_image])
     course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name="icons")
 
 
 class Teacher(models.Model):
     full_name = models.CharField(max_length=96)
     specialty = models.CharField(max_length=96)
-    photo = models.ImageField(upload_to="teacher_photos/")
+    photo = models.ImageField(upload_to="teacher_photos/", validators=[validate_image])
 
 
 class BannerImage(models.Model):
-    image = models.ImageField(upload_to="banner_images/")
+    image = models.ImageField(upload_to="banner_images/", validators=[validate_image])
 
 
 class Partner(models.Model):
-    logo = models.ImageField(upload_to="partners/")
+    logo = models.ImageField(upload_to="partners/" , validators=[validate_image])
     title = models.CharField(max_length=96)
 
 
---
+++ main/validators.py
@@ -0,0 +1,17 @@
+from PIL import Image
+from django.core.exceptions import ValidationError
+
+def validate_image(image):
+    # 1) Haqiqiy rasm ekanini tekshiramiz
+    try:
+        img = Image.open(image)
+        img.verify()
+    except Exception:
+        raise ValidationError("Image file is invalid!")
+
+    # 2) Rasm hajmini tekshiramiz
+    max_size_mb = 15
+    max_size = max_size_mb * 1024 * 1024 
+
+    if image.size > max_size:
+        raise ValidationError(f"Image size must not exceed {max_size_mb} MB!")
